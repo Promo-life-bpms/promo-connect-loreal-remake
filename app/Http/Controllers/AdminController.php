bc1a8f00cfbca82783806083540f2880b9c37e7d
Oscar-CR <krz.oficial@gmail.com>
2024-06-11 14:11:12 -0600

    agregado de funciones a controladores

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class AdminController extends Controller
 {
-    //
+    public function changeManualPassword(Request $request) {
+
+        $request->validate([
+            'password' => 'required|string',
+        ]);
+    
+        User::where('id', $request->user_id)->update([
+            'password'=>Hash::make($request->password)
+        ]);
+       
+        return back()->with('msg', 'Contraseña actualizada correctamente');
+    }
+
+    public function changeAutomaticPassword(Request $request) {
+
+        $randomPassword = Str::random(12); 
+
+        User::where('id', $request->user_id)->update([
+            'password'=>Hash::make($randomPassword)
+        ]);
+        return back()->with('msg', 'Contraseña actualizada correctamente');
+    }
 }
